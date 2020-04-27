f4e55dcd56ef292765da5fbcbf934fa9b775c0ad
Soham Banerjee <sohambanerjee4abc@gmail.com>
2020-04-27 20:09:27 +0530

    routing errors fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ArticleListComponent } from './article-list/article-list.component';
-
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
   { path :'articles',component: ArticleListComponent},
-  {path:'about', component:ArticleListComponent},
+  {path:'about', component:AboutComponent},
   { path :'',component: ArticleListComponent},
   
 ];
